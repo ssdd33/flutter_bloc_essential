e919330dae0cea26534d2b64c47f653ffae81de0
ssdd33 <beeblu.0@gmail.com>
2024-01-14 20:33:44 +0900

    reset master

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,4 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_block/bloc/theme/theme_bloc.dart';
 
 void main() {
   runApp(MyApp());
@@ -13,18 +9,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<ThemeBloc>(
-      create: (context) => ThemeBloc(),
-      child: Builder(builder: (context) {
-        return MaterialApp(
-          title: 'Event Payload',
-          debugShowCheckedModeBanner: false,
-          theme: context.watch<ThemeBloc>().state.appTheme == AppTheme.light
-              ? ThemeData.light()
-              : ThemeData.dark(),
-          home: const MyHomePage(),
-        );
-      }),
+    return MaterialApp(
+      title: 'Event Payload',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: const MyHomePage(),
     );
   }
 }
@@ -44,12 +33,7 @@ class MyHomePage extends StatelessWidget {
             'change Theme',
             style: TextStyle(fontSize: 24),
           ),
-          onPressed: () {
-            int randomInt = Random().nextInt(10);
-            print(randomInt);
-
-            context.read<ThemeBloc>().add(ChangeThemeEvent(ranInt: randomInt));
-          },
+          onPressed: () {},
         ),
       ),
     );
